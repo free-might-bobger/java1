8bd955161600a00fa61918d209ca4758fb2eba7f
Bobby Gerez <bobby.gerez@monstar-lab.com>
2023-10-03 15:56:13 +0800

    Initialize java

---
+++ src/main/java/in/ashokit/Product.java
@@ -0,0 +1,16 @@
+package in.ashokit;
+
+
+
+public class Product {
+	
+	public Integer id;
+	public String name;
+	public Integer price;
+	
+	public Product(Integer id, String name, Integer price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+}
